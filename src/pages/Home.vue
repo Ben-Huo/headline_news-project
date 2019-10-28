392eacaf752b9b135371c916c4ef333f057de950
Ben-Huo <641838789@qq.com>
2019-10-28 20:06:07 +0800

    创建首页页面

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <router-link to="/profile">个人中心</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,17 @@ import Router from "vue-router";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Profile from "../pages/Profile";
+import Home from "../pages/Home";
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path:'/',
+      name:'Home',
+      component:Home
+    },
     {
       path: "/login",
       name: "Login",
