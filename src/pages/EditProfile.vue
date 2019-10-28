d9e9a31e4fdb61025441f61188667549c94430f3
Ben-Huo <641838789@qq.com>
2019-10-28 21:14:53 +0800

    编辑资料页面的基本架构

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -1,20 +1,34 @@
 <template>
   <div>
       <headerMiddle title="编辑资料"></headerMiddle>
+      <div class="avatar">
+        <img src="../../static/images/pic.jpg" class="avatar-img">
+      </div>
+      <cellBar label="昵称" desc="柳东龙"/>
+      <cellBar label="密码" desc="*****"/>
+      <cellBar label="性别" desc="男"/>
   </div>
 </template>
 
 <script>
-import headerMiddle from '../components/headerMiddle'
+import headerMiddle from '../components/headerMiddle';
+import cellBar from '../components/cellBar';
+
 export default {
   components:{
-    headerMiddle : headerMiddle
+    headerMiddle,
+    cellBar
   }
-    
-  
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .avatar{
+    padding: 8.333vw;
+    text-align: center;
+  }
+  .avatar-img{
+    width: 19.444vw;
+    border-radius: 50%;
+  }
 </style>
