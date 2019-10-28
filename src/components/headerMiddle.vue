691c2094e67e9e6da06abb3c889fab457fab6202
Ben-Huo <641838789@qq.com>
2019-10-28 21:03:37 +0800

    封装头部组件

---
+++ src/components/headerMiddle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+      <span class="iconfont iconjiantou2"></span>
+      <div class="title">{{title}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['title']
+}
+</script>
+
+<style lang="less" scoped>
+    .header{
+        display: flex;
+        justify-content: space-between;
+        height: 50px;
+        line-height: 50px;
+    }
+    .iconfont{
+        padding:  0 2.778vw;
+    }
+    .title{
+        flex: 1;
+        text-align: center;
+        margin-right: 5vw;
+        font-weight: bold;
+        font-size: 18px;
+    }
+</style>
--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-      123456
+      <headerMiddle title="编辑资料"></headerMiddle>
   </div>
 </template>
 
 <script>
+import headerMiddle from '../components/headerMiddle'
 export default {
-
+  components:{
+    headerMiddle : headerMiddle
+  }
+    
+  
 }
 </script>
 
