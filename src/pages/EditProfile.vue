9bae3a14772bdcdb6a4cc9886ed654c29cfcb73d
Ben-Huo <641838789@qq.com>
2019-10-29 19:44:07 +0800

    实现昵称的修改以及封装数据加载函数

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -11,11 +11,11 @@
     <cellBar label="密码" desc="*****" />
     <cellBar label="性别" :desc="profile.gender" />
 
-    <van-dialog v-model="isShowNickname" title="编辑昵称" show-cancel-button>
+    <!-- v-model 控制是否显示  title  控制标题   show-cancel-button  控制是否显示取消确认按钮   confirm  是点击确认按钮事件 -->
+    <van-dialog v-model="isShowNickname" title="编辑昵称" show-cancel-button @confirm="editNickname">
       <!-- 这里要嵌入一个输入框 -->
       <van-field v-model="newNickname" placeholder="请输入昵称" />
     </van-dialog>
-    
   </div>
 </template>
 
@@ -35,24 +35,47 @@ export default {
       profile: {}
     };
   },
+  methods: {
+    loadPage() {
+      // 发送ajax请求
+      this.$axios({
+        url: "/user/" + localStorage.getItem("user_id"),
+        method: "get",
+        headers: {
+          Authorization: localStorage.getItem("token")
+        }
+      }).then(res => {
+        console.log(res);
+        // 数据获取完毕，用户数据在res.data.data里面
+        this.profile = res.data.data;
+        console.log(this.profile);
+        this.profile.gender = this.profile.gender == 1 ? "男" : "女";
+      });
+    },
+    editNickname() {
+      // 获取到了新昵称  this.newNickname
+      console.log("点击了确认按钮", this.newNickname);
+      this.$axios({
+        url: "/user_update/" + localStorage.getItem("user_id"),
+        method: "post",
+        headers: {
+          Authorization: localStorage.getItem("token")
+        },
+        data: {
+          nickname: this.newNickname
+        }
+      }).then(res => {
+        console.log(res.data);
+        this.loadPage()
+      });
+    }
+  },
   // 新的生命周期钩子函数 created()
   // 跟 mounted()  的区别在于他是这个组件实例创建完毕之后马上执行，这时候还未挂载，模板当中的那些dom都还不能使用
   // 可以用来获取数据
   mounted() {
-    // 发送ajax请求
-    this.$axios({
-      url: "/user/" + localStorage.getItem("user_id"),
-      method: "get",
-      headers: {
-        Authorization: localStorage.getItem("token")
-      }
-    }).then(res => {
-      console.log(res);
-      // 数据获取完毕，用户数据在res.data.data里面
-      this.profile = res.data.data;
-      console.log(this.profile);
-      this.profile.gender = this.profile.gender == 1 ? "男" : "女";
-    });
+    // 将读取数据的方法封装起来，每一次修改都重新读取一遍数据
+    this.loadPage()
   }
 };
 </script>
