d1931319c97a342c784b4b7c290a0a7aaf942ede
Ben-Huo <641838789@qq.com>
2019-10-30 21:53:22 +0800

    我的关注页面布局完成

--- src/pages/MyFollows.vue
+++ src/pages/MyFollows.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-      123345
+      <headerMiddle title="我的关注"/>
+      <div class="item">
+          <!-- 这里是关注的框  用 v-for 遍历数据 -->
+          <img src="/static/images/pic.jpg" class="avatar">
+          <div class="itemMiddle">
+              <div class="name">火星新闻晚班</div>
+              <div class="data">2019-10-4</div>
+          </div>
+          <div class="btnCancel">取消关注</div>
+      </div>
   </div>
 </template>
 
 <script>
+import headerMiddle from '../components/headerMiddle'
 export default {
-
+    components:{
+        headerMiddle
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .item{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 5.556vw;
+        border-bottom: 1px solid #eee;
+    }
+    .avatar{
+        width: 11.111vw;
+        border-radius: 50%
+    }
+    .itemMiddle{
+        flex: 1;
+        padding-left: 4.167vw;
+        .data{
+            font-size: 3.889vw;
+            color: #888;
+        }
+    }
+    .btnCancel{
+        height: 8.333vw;
+        line-height: 8.333vw;
+        padding:  0 2.778vw;
+        border-radius: 20px;
+        background-color: #eee;
+        color: #888;
+    }
 </style>
