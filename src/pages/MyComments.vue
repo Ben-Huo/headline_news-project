c4b5a20feb6b346b7f483a2bc658ac5f981f7997
Ben-Huo <641838789@qq.com>
2019-10-30 22:47:39 +0800

    我的跟帖页面构建完成

--- src/pages/MyComments.vue
+++ src/pages/MyComments.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
       <headerMiddle title="我的跟帖"/>
+      <div class="item" v-for="(item,index) in CommentsList" :key="index">
+          <div class="data">2019-10-4</div>
+          <div class="parent" v-if="item.parent">
+            <div class="name">回复：{{item.parent.user.nickname}}</div>
+            <div class="parentContent">
+                {{item.parent.content}}
+            </div>
+          </div>
+          <div class="mainContent">
+              {{item.content}}
+          </div>
+          <div class="postOrigin">
+              原文：{{item.post.title}}
+          </div>
+      </div>
   </div>
 </template>
 
@@ -9,10 +24,47 @@ import headerMiddle from '../components/headerMiddle'
 export default {
     components:{
         headerMiddle
+    },
+    data(){
+        return{
+            CommentsList:[]
+        }
+    },
+    mounted(){
+        this.$axios({
+            url:'/user_comments',
+            method:'get'
+        }).then(res=>{
+            console.log(res.data);
+            this.CommentsList=res.data.data
+        })
     }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .item{
+        font-size: 3.889vw;
+        padding: 5.556vw;
+        border-bottom: 1px solid #eee;
+    }
+    .data{
+        color: #888;
+    }
+    .postOrigin{
+        margin-top: 2.778vw;        
+        color: #888;
+    }
+    .parent{
+        margin-top: 2.778vw;
+        color: #888;
+        padding: 4.167vw;
+        background-color: #eee;
+        .parentContent{
+        margin-top: 2.778vw;
+        }
+    }
+    .mainContent{
+        margin-top: 2.778vw;
+    }
 </style>
