8712ad33380d9357cdf3192bc5f240c25d92f580
Ben-Huo <641838789@qq.com>
2019-10-31 12:31:53 +0800

    初步建立tab栏组件

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
     <homeHeader />
+
+    <!-- 顶部tab组件 -->
+    <van-tabs v-model="active">
+      <van-tab v-for="(item,index) in tabList" :key="index" :title="item.name">内容 {{item.name}}</van-tab>
+
+    </van-tabs>
   </div>
 </template>
 
 <script>
-import homeHeader from '../components/homeHeader'
+import homeHeader from "../components/homeHeader";
 export default {
-  components:{
+  components: {
     homeHeader
+  },
+  data(){
+    return{
+      active:0,
+      tabList:[]
+    }
+  },
+  mounted(){
+    this.$axios({
+      url:'/category',
+      method:'get'
+    }).then(res=>{
+      console.log(res.data);
+      this.tabList = res.data.data
+    })
   }
-}
+};
 </script>
 
 <style>
-
 </style>
