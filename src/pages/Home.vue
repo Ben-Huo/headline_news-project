72b89291d9817a62fe327ad4379e4d84ba5994a9
Ben-Huo <641838789@qq.com>
2019-10-31 14:16:31 +0800

    tab栏以显示头条为默认分类

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,6 @@
     <!-- 顶部tab组件 -->
     <van-tabs v-model="active">
       <van-tab v-for="(item,index) in tabList" :key="index" :title="item.name">内容 {{item.name}}</van-tab>
-
     </van-tabs>
   </div>
 </template>
@@ -16,20 +15,26 @@ export default {
   components: {
     homeHeader
   },
-  data(){
-    return{
-      active:0,
-      tabList:[]
-    }
+  data() {
+    return {
+      // 获取 token 就可以判断有没有登陆，设置一个默认激活的当前分类
+      active: localStorage.getItem('token')?1:0,
+      tabList: []
+    };
   },
-  mounted(){
-    this.$axios({
-      url:'/category',
-      method:'get'
-    }).then(res=>{
-      console.log(res.data);
-      this.tabList = res.data.data
-    })
+  mounted() {
+    this.initTabList();
+  },
+  methods: {
+    initTabList() {
+      this.$axios({
+        url: "/category",
+        method: "get"
+      }).then(res => {
+        console.log(res.data);
+        this.tabList = res.data.data;
+      });
+    }
   }
 };
 </script>
